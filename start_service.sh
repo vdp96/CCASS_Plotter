08c6928908c95e865bfe070d43343c3f3552ff05
vdp96 <vpunjala1996@gmail.com>
2022-05-14 19:20:39 +0530

    changing env

--- start_service.sh
+++ start_service.sh
@@ -2,5 +2,6 @@ export FLASK_ENV=development
 
 # source activate ~/opt/anaconda3/envs/conda_sample
 
-pyt=~/opt/anaconda3/envs/conda_sample/bin/python/
+# pyt=~/opt/anaconda3/envs/conda_sample/bin/python/
+pyt=~/home/ls/envs/py39/bin/python/
 $pyt ~/projects/CCASS_Plotter/app.py
